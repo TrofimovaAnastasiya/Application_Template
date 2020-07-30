85aa305ab2f1327fd5cb1536f7f1a9a42ff48143
Nastya Trofimova <trofimova.anastasiya.0312@gmail.com>
2020-07-30 22:08:24 +0700

    creating a template for header

--- src/JavaScripts/index.js
+++ src/JavaScripts/index.js
@@ -1,3 +1,19 @@
-console.log('Works')
-import '../SCSS/index.scss'
-import './module'
+console.log("Works");
+import "../SCSS/index.scss";
+import "./module";
+
+//функция вставки нового элемента при случившемся событии
+let i = 0;
+document //обращение к документу
+  .querySelector(".header__buttons-registration") //поиск элемента "класс(id)" в документе
+  .addEventListener("click", (event) => {
+    // прослушка события "клик", => после события выполняется код
+    let el = document.createElement("div"); //инициализация переменной el, которая создает элемент "тег"
+    el.setAttribute("class", "header__nav-item"); // выбор атрибута "class(alt,href)" и его наименование
+    el.innerHTML = `                              
+  <a class="nav-item__link" href="#!">${i}</a>
+`; //вставка HTML-кода
+    //alert("click123");
+    i++;
+    document.querySelector(".header__buttons-registration").appendChild(el); //поиск элемента "класс" в документе и вставка HTML кода из переменной el в его дочерний элемент
+  });
--- src/JavaScripts/module.js
+++ src/JavaScripts/module.js
@@ -1,7 +1,1 @@
-console.log("module");
 
-async function start() {
-  let = "async works !";
-  return await Promise.resolve(st);
-}
-start().then(console.log);
